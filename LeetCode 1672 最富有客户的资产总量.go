0a5300152f990a802bd32986b6b8a8f6b4961081
zaizai <327120052@qq.com>
2022-07-08 20:27:55 +0800

    20220708 LeetCode 1672 最富有客户的资产总量

--- LeetCode 1672 最富有客户的资产总量.go
+++ LeetCode 1672 最富有客户的资产总量.go
@@ -4,7 +4,7 @@ func maximumWealth(accounts [][]int) int {
 	ans := 0
 	for i := 0; i < len(accounts); i++ {
 		sum := 0
-		for j := 0; j < len(accounts); j++ {
+		for j := 0; j < len(accounts[i]); j++ {
 			sum += accounts[i][j]
 		}
 		if sum > ans {
