9dd594524639f9156e03685c08de360813cf5422
zaizai <327120052@qq.com>
2022-09-07 10:00:36 +0800

    20220907 LeetCode 0339 嵌套列表权重和

---
+++ LeetCode 0339 嵌套列表权重和.go
@@ -0,0 +1,43 @@
+package main
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * type NestedInteger struct {
+ * }
+ *
+ * // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ * func (n NestedInteger) IsInteger() bool {}
+ *
+ * // Return the single integer that this NestedInteger holds, if it holds a single integer
+ * // The result is undefined if this NestedInteger holds a nested list
+ * // So before calling this method, you should have a check
+ * func (n NestedInteger) GetInteger() int {}
+ *
+ * // Set this NestedInteger to hold a single integer.
+ * func (n *NestedInteger) SetInteger(value int) {}
+ *
+ * // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ * func (n *NestedInteger) Add(elem NestedInteger) {}
+ *
+ * // Return the nested list that this NestedInteger holds, if it holds a nested list
+ * // The list length is zero if this NestedInteger holds a single integer
+ * // You can access NestedInteger's List element directly if you want to modify it
+ * func (n NestedInteger) GetList() []*NestedInteger {}
+ */
+
+func dfs(list []*NestedInteger, depth int) int {
+	sum := 0
+	for _, n := range list {
+		if n.IsInteger() {
+			sum += n.GetInteger() * depth
+		} else {
+			sum += dfs(n.GetList(), depth+1)
+		}
+	}
+	return sum
+}
+
+func depthSum(nestedList []*NestedInteger) int {
+	return dfs(nestedList, 1)
+}
