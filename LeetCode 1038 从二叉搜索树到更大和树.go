bc74c8197e758c7031988bfc3105cbc927a08e3c
zaizai <327120052@qq.com>
2022-10-10 10:01:24 +0800

    20221010 LeetCode 1038 从二叉搜索树到更大和树

---
+++ LeetCode 1038 从二叉搜索树到更大和树.go
@@ -0,0 +1,24 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func bstToGst(root *TreeNode) *TreeNode {
+	sum := 0
+	var dfs func(node *TreeNode)
+	dfs = func(node *TreeNode) {
+		if node != nil {
+			dfs(node.Right)
+			sum += node.Val
+			node.Val = sum
+			dfs(node.Left)
+		}
+	}
+	dfs(root)
+	return root
+}
