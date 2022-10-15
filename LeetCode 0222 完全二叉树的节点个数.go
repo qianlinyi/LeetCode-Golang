0ad5d545226bbf99fc48f50172511da23a711f61
zaizai <327120052@qq.com>
2022-10-15 10:36:46 +0800

    20221015 LeetCode 0222 完全二叉树的节点个数

---
+++ LeetCode 0222 完全二叉树的节点个数.go
@@ -0,0 +1,23 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+// DFS
+func countNodes(root *TreeNode) int {
+	var dfs func(node *TreeNode) int
+	dfs = func(node *TreeNode) int {
+		if node == nil {
+			return 0
+		}
+		ans := 1 + dfs(node.Left) + dfs(node.Right)
+		return ans
+	}
+	return dfs(root)
+}
