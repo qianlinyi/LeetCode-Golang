8100a59fc6bf7265616fe8097b1737e93511744a
zaizai <327120052@qq.com>
2022-12-14 16:40:49 +0800

    20221214 LeetCode 0108 将有序数组转换为二叉搜索树 DFS

---
+++ LeetCode 0108 将有序数组转换为二叉搜索树.go
@@ -0,0 +1,18 @@
+package main
+
+func sortedArrayToBST(nums []int) *TreeNode {
+	return helper(nums, 0, len(nums)-1)
+}
+
+func helper(nums []int, left, right int) *TreeNode {
+	if left > right {
+		return nil
+	}
+
+	// 总是选择中间位置右边的数字作为根节点
+	mid := (left + right + 1) / 2
+	root := &TreeNode{Val: nums[mid]}
+	root.Left = helper(nums, left, mid-1)
+	root.Right = helper(nums, mid+1, right)
+	return root
+}
